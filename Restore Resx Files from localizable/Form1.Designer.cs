722292917e7e003abbb1a8e97536ab916545df49
denalekseev@gmail.com <denalekseev@gmail.com>
2018-10-24 11:16:26 +0300

    initial

---
+++ Restore Resx Files from localizable/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Restore_Resx_Files_from_localizable
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.RunBtn = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.SourcePathEdit = new System.Windows.Forms.TextBox();
+			this.DestPathEdit = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SelectSourceBtn = new System.Windows.Forms.Button();
+			this.SelectDestBtn = new System.Windows.Forms.Button();
+			this.ResultBox = new System.Windows.Forms.RichTextBox();
+			this.ResultGroupBox = new System.Windows.Forms.GroupBox();
+			this.ParamsGroupBox = new System.Windows.Forms.GroupBox();
+			this.ResultGroupBox.SuspendLayout();
+			this.ParamsGroupBox.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// RunBtn
+			// 
+			this.RunBtn.Location = new System.Drawing.Point(53, 74);
+			this.RunBtn.Name = "RunBtn";
+			this.RunBtn.Size = new System.Drawing.Size(75, 23);
+			this.RunBtn.TabIndex = 0;
+			this.RunBtn.Text = "Run";
+			this.RunBtn.UseVisualStyleBackColor = true;
+			this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// SourcePathEdit
+			// 
+			this.SourcePathEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.SourcePathEdit.Location = new System.Drawing.Point(53, 22);
+			this.SourcePathEdit.Name = "SourcePathEdit";
+			this.SourcePathEdit.Size = new System.Drawing.Size(650, 20);
+			this.SourcePathEdit.TabIndex = 1;
+			// 
+			// DestPathEdit
+			// 
+			this.DestPathEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.DestPathEdit.Location = new System.Drawing.Point(53, 48);
+			this.DestPathEdit.Name = "DestPathEdit";
+			this.DestPathEdit.Size = new System.Drawing.Size(650, 20);
+			this.DestPathEdit.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(39, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "source";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(27, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "dest";
+			// 
+			// SelectSourceBtn
+			// 
+			this.SelectSourceBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.SelectSourceBtn.Location = new System.Drawing.Point(709, 20);
+			this.SelectSourceBtn.Name = "SelectSourceBtn";
+			this.SelectSourceBtn.Size = new System.Drawing.Size(75, 23);
+			this.SelectSourceBtn.TabIndex = 5;
+			this.SelectSourceBtn.Text = "...";
+			this.SelectSourceBtn.UseVisualStyleBackColor = true;
+			this.SelectSourceBtn.Click += new System.EventHandler(this.SelectSourceBtn_Click);
+			// 
+			// SelectDestBtn
+			// 
+			this.SelectDestBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.SelectDestBtn.Location = new System.Drawing.Point(709, 49);
+			this.SelectDestBtn.Name = "SelectDestBtn";
+			this.SelectDestBtn.Size = new System.Drawing.Size(75, 23);
+			this.SelectDestBtn.TabIndex = 6;
+			this.SelectDestBtn.Text = "...";
+			this.SelectDestBtn.UseVisualStyleBackColor = true;
+			this.SelectDestBtn.Click += new System.EventHandler(this.SelectDestBtn_Click);
+			// 
+			// ResultBox
+			// 
+			this.ResultBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ResultBox.Location = new System.Drawing.Point(8, 21);
+			this.ResultBox.Name = "ResultBox";
+			this.ResultBox.Size = new System.Drawing.Size(784, 314);
+			this.ResultBox.TabIndex = 7;
+			this.ResultBox.Text = "";
+			// 
+			// ResultGroupBox
+			// 
+			this.ResultGroupBox.Controls.Add(this.ResultBox);
+			this.ResultGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ResultGroupBox.Location = new System.Drawing.Point(0, 107);
+			this.ResultGroupBox.Name = "ResultGroupBox";
+			this.ResultGroupBox.Padding = new System.Windows.Forms.Padding(8);
+			this.ResultGroupBox.Size = new System.Drawing.Size(800, 343);
+			this.ResultGroupBox.TabIndex = 8;
+			this.ResultGroupBox.TabStop = false;
+			this.ResultGroupBox.Text = "Results";
+			// 
+			// ParamsGroupBox
+			// 
+			this.ParamsGroupBox.Controls.Add(this.DestPathEdit);
+			this.ParamsGroupBox.Controls.Add(this.RunBtn);
+			this.ParamsGroupBox.Controls.Add(this.SelectDestBtn);
+			this.ParamsGroupBox.Controls.Add(this.SourcePathEdit);
+			this.ParamsGroupBox.Controls.Add(this.SelectSourceBtn);
+			this.ParamsGroupBox.Controls.Add(this.label1);
+			this.ParamsGroupBox.Controls.Add(this.label2);
+			this.ParamsGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
+			this.ParamsGroupBox.Location = new System.Drawing.Point(0, 0);
+			this.ParamsGroupBox.Name = "ParamsGroupBox";
+			this.ParamsGroupBox.Size = new System.Drawing.Size(800, 107);
+			this.ParamsGroupBox.TabIndex = 9;
+			this.ParamsGroupBox.TabStop = false;
+			this.ParamsGroupBox.Text = "Parameters";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.ResultGroupBox);
+			this.Controls.Add(this.ParamsGroupBox);
+			this.Name = "Form1";
+			this.Text = "-";
+			this.ResultGroupBox.ResumeLayout(false);
+			this.ParamsGroupBox.ResumeLayout(false);
+			this.ParamsGroupBox.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button RunBtn;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.TextBox SourcePathEdit;
+		private System.Windows.Forms.TextBox DestPathEdit;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button SelectSourceBtn;
+		private System.Windows.Forms.Button SelectDestBtn;
+		private System.Windows.Forms.RichTextBox ResultBox;
+		private System.Windows.Forms.GroupBox ResultGroupBox;
+		private System.Windows.Forms.GroupBox ParamsGroupBox;
+	}
+}
+
---
+++ Restore Resx Files from localizable/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
+
+namespace Restore_Resx_Files_from_localizable
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void SelectSourceBtn_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(SourcePathEdit.Text))
+			{
+				openFileDialog1.InitialDirectory = System.IO.Path.GetDirectoryName(SourcePathEdit.Text);
+			}
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				SourcePathEdit.Text = openFileDialog1.FileName;
+
+				if (!string.IsNullOrEmpty(openFileDialog1.FileName) && openFileDialog1.FileName.Contains(".ru"))
+				{
+					DestPathEdit.Text = openFileDialog1.FileName.Replace(".ru", string.Empty);
+				}
+			}
+		}
+
+		private void SelectDestBtn_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(DestPathEdit.Text))
+			{
+				saveFileDialog1.InitialDirectory = System.IO.Path.GetDirectoryName(DestPathEdit.Text);
+			}
+
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				DestPathEdit.Text = saveFileDialog1.FileName;
+			}
+		}
+
+		IDictionary<string, object> destValues = new Dictionary<string, object>();
+		IDictionary<string, object> destMetaValues = new Dictionary<string, object>();
+		IDictionary<string, object> sourceValues = new Dictionary<string, object>();
+
+		private void RunBtn_Click(object sender, EventArgs e)
+		{
+			var lst = new List<long>();
+
+			ResultBox.Text = string.Empty;
+
+			destValues.Clear();
+			sourceValues.Clear();
+			destMetaValues.Clear();
+
+			var modules = AppDomain.CurrentDomain.GetAssemblies().Select(m => m.GetName()).ToArray();
+
+			using (ResXResourceReader resxReader = new ResXResourceReader(DestPathEdit.Text, modules))
+			{
+				foreach (DictionaryEntry entry in resxReader)
+				{
+					destValues.Add((string)entry.Key, entry.Value);
+				}
+
+				IDictionaryEnumerator metadataEnumerator = resxReader.GetMetadataEnumerator();
+
+				while (metadataEnumerator.MoveNext())
+				{
+					destMetaValues.Add((string)metadataEnumerator.Entry.Key, metadataEnumerator.Entry.Value);
+				}
+
+			}
+
+
+			using (ResXResourceReader resxReader = new ResXResourceReader(SourcePathEdit.Text, modules))
+			{
+				foreach (DictionaryEntry entry in resxReader)
+				{
+					sourceValues.Add((string)entry.Key, entry.Value);
+				}
+			}
+
+			var countNew = 0;
+			var countReplaced = 0;
+			var countUnchanged = 0;
+
+			using (ResXResourceWriter resx = new ResXResourceWriter(DestPathEdit.Text))
+			{
+				foreach (var a in destValues)
+				{
+					var value = a.Value;
+
+					var forceUseDest = (a.Key.EndsWith(".Text") || a.Key.EndsWith(".Caption")) && a.Value != null && !string.IsNullOrWhiteSpace(a.Value.ToString());
+
+					if (!forceUseDest && sourceValues.ContainsKey(a.Key))
+					{
+						resx.AddResource(a.Key, sourceValues[a.Key]);
+						countReplaced += 1;
+					}
+					else
+					{
+						resx.AddResource(a.Key, a.Value);
+						countUnchanged += 1;
+					}
+				}
+
+				foreach (var a in destMetaValues)
+				{
+					resx.AddMetadata(a.Key, a.Value);
+				}
+
+
+				foreach (var a in sourceValues)
+				{
+					if (!destValues.ContainsKey(a.Key))
+					{
+						if ((a.Key.EndsWith(".Text") || a.Key.EndsWith(".Caption")) && a.Value != null && !string.IsNullOrWhiteSpace(a.Value.ToString()))
+						{
+							resx.AddResource(a.Key, "_" + a.Value.ToString());
+						}
+						else
+						{
+							resx.AddResource(a.Key, a.Value);
+						}
+
+						countNew++;
+					}
+				}
+				resx.Generate();
+
+				ResultBox.Text += string.Format("Count new added elements from source resx file = {0}{1}", countNew, Environment.NewLine);
+				ResultBox.Text += string.Format("Count replaced elements from source resx file = {0}{1}", countReplaced, Environment.NewLine);
+				ResultBox.Text += string.Format("Count unchanged elements in dest resx file = {0}{1}", countUnchanged, Environment.NewLine);
+			}
+		}
+	}
+}
